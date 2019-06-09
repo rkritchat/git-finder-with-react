c1ab410258a8c09c9f24bc22ac362e788aaeac9d
rkritchat <rkritchat@gmail.com>
2019-06-09 12:52:46 +0700

    Add about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Navbar from "./components/layout/Navbar"
 import Users from "./components/user/Users"
 import axios from "axios"
 import Search from "./components/user/Search"
 import Alert from "./components/layout/Alert"
+import About from "./components/pages/About"
 
 class App extends Component {
   state = {
@@ -47,19 +49,32 @@ class App extends Component {
   render() {
     const { users, loading, alert } = this.state
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
