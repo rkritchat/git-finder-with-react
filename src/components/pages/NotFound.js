ba1e062aedfc6ec6860337e7adbd86bffd905cb3
rkritchat <rkritchat@gmail.com>
2019-06-16 17:21:43 +0700

    Add home and not found page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <div className=''>
+      <h1>Not Found</h1>
+      <p className='lead'>The page that toy are looking for does not exist</p>
+    </div>
+  )
+}
+
+export default NotFound
